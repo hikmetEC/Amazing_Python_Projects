a48c71beaafb59dd75e8553a182b692d043ee395
Hikmet Efe Canikli <efsiprocanik@gmail.com>
2023-08-11 15:35:19 +0300

    Update countdown_clock.py
    +Countdown clock fix

--- countdown_clock.py
+++ countdown_clock.py
@@ -2,18 +2,24 @@ import time
 
 print("To stop input X")
 global timerInput
+
 class Clock():
-    def StartClock():
+    def StartClock(timerInput):
         counterTime = time.time()
-        while counterTime+timerInput < time.time():
-            if input() == "X":
-                break; 
-            print(timerInput-(time.time()-counterTime))
+        while counterTime+timerInput > time.time():
+            print(round(timerInput-(time.time()-counterTime),2))
+            
         print("Timer Finished")
         Clock.StartProgram()
 
     def StartProgram():
+        if(input("Do you want to exit?(Y/N)): ") == "Y"): 
+            exit()
+
         timerInput = int(input("Timer Length (in seconds): "))
+        
+        if(timerInput == "X"): 
+            exit()
         Clock.StartClock(timerInput)
 
 Clock.StartProgram()
